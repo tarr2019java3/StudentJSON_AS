7dc5d4d197ff5318b57977a5801ef18db4f812c4
Adam <ajjstepien@gmail.com>
2020-02-29 12:00:59 +0100

    dodano listę student

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -17,7 +18,16 @@ public class Main {
         Student student4 = new Student("Monika", "Brodka", 14);
         Student student5 = new Student("Wiesław", "Zięba", 15);
 
-        ObjectMapper mapper = new ObjectMapper();                                          // serializujemy do formatu JSON
+        ObjectMapper mapper = new ObjectMapper();   // serializujemy do formatu JSON
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student);
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+        studentList.add(student5);
+
         try {
             mapper.writeValue(new File("student.json"), student);
             mapper.writeValue(new File("student1.json"), student1);
@@ -25,12 +35,13 @@ public class Main {
             mapper.writeValue(new File("student3.json"), student3);
             mapper.writeValue(new File("student4.json"), student4);
             mapper.writeValue(new File("student5.json"), student5);
+            mapper.writeValue(new File("studentList.json"), studentList);
         } catch (JsonMappingException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        
+
     }
 }
