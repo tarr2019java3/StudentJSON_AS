678f6d993f8fbc030a355ca646a9f8671b21d94c
Adam <ajjstepien@gmail.com>
2020-02-29 11:49:30 +0100

    dodano 5 obiektów typu Student

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -5,18 +5,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Student student = new Student("Jan", "Kowalski", 32); //serializacja - przekształcenie obiektu
+        Student student = new Student("Jan", "Kowalski", 10); //serializacja - przekształcenie obiektu
+        Student student1 = new Student("Marian", "Banaś", 11);
+        Student student2 = new Student("Zenek", "Martyniuk", 12);
+        Student student3 = new Student("Jan", "Szlaga", 13);
+        Student student4 = new Student("Monika", "Brodka", 14);
+        Student student5 = new Student("Wiesław", "Zięba", 15);
+
         ObjectMapper mapper = new ObjectMapper();                                          // serializujemy do formatu JSON
         try {
             mapper.writeValue(new File("student.json"), student);
+            mapper.writeValue(new File("student1.json"), student1);
+            mapper.writeValue(new File("student2.json"), student2);
+            mapper.writeValue(new File("student3.json"), student3);
+            mapper.writeValue(new File("student4.json"), student4);
+            mapper.writeValue(new File("student5.json"), student5);
         } catch (JsonMappingException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        
     }
 }
