0b603ec5ff7d4738fc99e5a8169d11bb1e838bb0
Adam <ajjstepien@gmail.com>
2020-02-29 11:36:01 +0100

    zaimportowano biblioteki jacson

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -1,8 +1,22 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Student student = new Student("Jan", "Kowalski", 32); //serializacja - przekształcenie obiektu
+        ObjectMapper mapper = new ObjectMapper();                                          // serializujemy do formatu JSON
+        try {
+            mapper.writeValue(new File("student.json"), student);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
